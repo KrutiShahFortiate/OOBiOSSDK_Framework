5ab135f1d4e8b59c0a8b78037680bfb8af98dc3c
KrutiShah_Fortiate <kruti@fortiate.com>
2022-05-31 00:08:26 +0530

    Initial Commit

---
+++ OOBiOSSDK/OOBiOSSDK.h
@@ -0,0 +1,18 @@
+//
+//  OOBiOSSDK.h
+//  OOBiOSSDK
+//
+//  Created by Kruti on 31/05/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OOBiOSSDK.
+FOUNDATION_EXPORT double OOBiOSSDKVersionNumber;
+
+//! Project version string for OOBiOSSDK.
+FOUNDATION_EXPORT const unsigned char OOBiOSSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OOBiOSSDK/PublicHeader.h>
+
+
